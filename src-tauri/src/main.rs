11747d9974485a43bb36bc636e347e49a25a480c
enpitsulin <enpitsulin@gmail.com>
2022-05-02 01:16:12 +0800

    new command new_todo toggle_done

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,7 +7,7 @@ use todo::{Todo, TodoApp};
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_todos])
+        .invoke_handler(tauri::generate_handler![get_todos, new_todo, toggle_done])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -18,4 +18,29 @@ fn get_todos() -> Vec<Todo> {
     let todos = app.get_todos().unwrap();
     app.conn.close();
     todos
+}
+
+#[tauri::command]
+fn new_todo(todo: Todo) -> bool {
+    let app = TodoApp::new().unwrap();
+    let result = app.new_todo(todo);
+    app.conn.close();
+    result
+}
+
+#[tauri::command]
+fn toggle_done(id: String) -> bool {
+    let app = TodoApp::new().unwrap();
+    let Todo {
+        id,
+        label,
+        done,
+        is_delete,
+    } = app.get_todo(id).unwrap();
+    app.update_todo(Todo {
+        id,
+        label,
+        done: !done,
+        is_delete,
+    })
 }
