9abecfef2322938c741551bc5e0403e3ca91e2ef
enpitsulin <enpitsulin@gmail.com>
2022-05-02 01:15:47 +0800

    more complete todo struct

--- src-tauri/src/todo.rs
+++ src-tauri/src/todo.rs
@@ -1,12 +1,12 @@
 use rusqlite::{Connection, Result};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 pub struct Todo {
-    id: String,
-    label: String,
-    done: bool,
-    is_delete: bool,
+    pub id: String,
+    pub label: String,
+    pub done: bool,
+    pub is_delete: bool,
 }
 
 pub struct TodoApp {
@@ -18,6 +18,20 @@ impl TodoApp {
         let db_path = "./db.sqlite";
         let conn = Connection::open(db_path)?;
         Ok(TodoApp { conn })
+    }
+
+    pub fn get_todo(&self, id: String) -> Result<Todo> {
+        let mut stmt = self.conn.prepare("SELECT * FROM todo WHERE id = ?")?;
+        let mut rows = stmt.query_map(&[&id], |row| {
+            Ok(Todo {
+                id: row.get(0)?,
+                label: row.get(1)?,
+                done: row.get(2)?,
+                is_delete: row.get(3)?,
+            })
+        })?;
+        let todo = rows.next().unwrap()?;
+        Ok(todo)
     }
 
     pub fn get_todos(&self) -> Result<Vec<Todo>> {
@@ -44,10 +58,10 @@ impl TodoApp {
 
     pub fn new_todo(&self, todo: Todo) -> bool {
         let Todo { id, label, .. } = todo;
-        match self.conn.execute(
-            "INSERT INTO Todo (id, label, done, is_delete) VALUES (?, ?, ?, ?)",
-            [id, label],
-        ) {
+        match self
+            .conn
+            .execute("INSERT INTO Todo (id, label) VALUES (?, ?)", [id, label])
+        {
             Ok(insert) => {
                 println!("{}", insert);
                 true
@@ -70,7 +84,7 @@ impl TodoApp {
         let is_delete = if is_delete == true { 1 } else { 0 };
         match self.conn.execute(
             "UPDATE Todo
-        SET label = ?1, done = ?2, is_delete = ?3 WHERE id = ?4",
+        SET label = ?1, done = ?2, isDelete = ?3 WHERE id = ?4",
             [label, done.to_string(), is_delete.to_string(), id],
         ) {
             Ok(update) => {
