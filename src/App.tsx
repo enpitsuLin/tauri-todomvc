b741244d3dc7042492dd09cd3c47e687e0f595a6
enpitsulin <enpitsulin@gmail.com>
2022-05-02 11:51:01 +0800

    get todo from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { invoke } from '@tauri-apps/api'
+import { useEffect, useState } from 'react'
 import TodoList from './component/TodoList'
-
+import { Todo } from './types/todo'
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([])
+  useEffect(() => {
+    invoke<Todo[]>('get_todos').then((res) => {
+      setTodos(res)
+    })
+  }, [])
   return (
     <div className="todoapp">
-      <TodoList />
+      <TodoList todos={todos} />
     </div>
   )
 }
