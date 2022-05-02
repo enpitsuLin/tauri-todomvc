0c40cdd0091fa3ab69e13c7c577e563bf1e3af78
enpitsulin <enpitsulin@gmail.com>
2022-05-02 11:59:22 +0800

    use jotai store

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { invoke } from '@tauri-apps/api'
-import { useEffect, useState } from 'react'
+import { useAtom } from 'jotai'
+import { useEffect } from 'react'
 import TodoList from './component/TodoList'
+import { allTodosAtom, todosAtom } from './store/todos'
 import { Todo } from './types/todo'
+
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([])
+  const [, setAllTodos] = useAtom(allTodosAtom)
+  const [todos] = useAtom(todosAtom)
   useEffect(() => {
     invoke<Todo[]>('get_todos').then((res) => {
-      setTodos(res)
+      setAllTodos(res)
     })
   }, [])
   return (
