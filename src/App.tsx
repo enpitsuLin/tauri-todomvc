26dff413b48a2afdf7040095afbf196dc39e0c31
enpitsulin <enpitsulin@gmail.com>
2022-05-02 14:07:40 +0800

    fix use wrong atom

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { invoke } from '@tauri-apps/api'
 import { useAtom } from 'jotai'
 import { useEffect } from 'react'
 import TodoList from './component/TodoList'
-import { allTodosAtom, todosAtom } from './store/todos'
+import { allTodosAtom, filterAtom } from './store/todos'
 import { Todo } from './types/todo'
 
 function App() {
   const [, setAllTodos] = useAtom(allTodosAtom)
-  const [todos] = useAtom(todosAtom)
+  const [todos] = useAtom(filterAtom)
   useEffect(() => {
     invoke<Todo[]>('get_todos').then((res) => {
       setAllTodos(res)
